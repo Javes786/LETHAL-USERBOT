830de8536a7c9221325e27e86b43ded40db776d0
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-09-02 21:18:33 +0530

    Update amaan.py

--- amaan.py
+++ amaan.py
@@ -1,5 +1,12 @@
 import os
+import logging
+#
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
+)
 
+
+#
 os.system("git clone https://github.com/Javes786/LETHAL-USERBOT && cd LETHAL-USERBOT && python3 -m Lethal")
 
 # if got error
