c5fd1a5422b91561c65d289f7b6aa70f2e8e418f
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-09-02 21:13:30 +0530

    Update amaan.py

--- amaan.py
+++ amaan.py
@@ -1,6 +1,6 @@
 import os
 
-os.system("git clone https://github.com/Javes786/LETHAL-USERBOT && cd LETHAL-USERBOT && python3 -m userbot")
+os.system("git clone https://github.com/Javes786/LETHAL-USERBOT && cd LETHAL-USERBOT && python3 -m Lethal")
 
 # if got error
 # print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @DestroyXSupport")
