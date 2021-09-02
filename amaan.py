a590c2e3c8aa2337456a73acf332513211215920
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-09-02 19:23:40 +0530

    Update and rename ShashankMd.py to amaan.py

---
+++ amaan.py
@@ -0,0 +1,10 @@
+import os
+
+os.system("https://github.com/Javes786/LETHAL-USERBOT && python3 -m Lethal")
+
+# if got error
+# print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @DestroyXSupport")
+# Script written for stop heroku suspension
+# use with credit else gay
+# Bhosdike Veete ooo vete mauJ Krdi Yarr TuM 
+# Toh Bade Heavy Driver NiKLe
