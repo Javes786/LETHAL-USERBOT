d8049528922f71a176a93003cdec433f365fe2a3
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-09-02 12:51:36 +0530

    Update termux_session_gen.sh

--- resources/termux_session_gen.sh
+++ resources/termux_session_gen.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 clear
 echo "
- _       _____   ____   _   _ 
-| |     |_   _| / __ \ | \ | |
-| |       | |  | |  | ||  \| |
-| |       | |  | |  | || . ` |
-| |____  _| |_ | |__| || |\  |
-|______||_____| \____/ |_| \_|
-                              
-                              
+
+╔╗──╔═══╦════╦╗─╔╦═══╦╗
+║║──║╔══╣╔╗╔╗║║─║║╔═╗║║
+║║──║╚══╬╝║║╚╣╚═╝║║─║║║
+║║─╔╣╔══╝─║║─║╔═╗║╚═╝║║─╔╗
+║╚═╝║╚══╗─║║─║║─║║╔═╗║╚═╝║
+╚═══╩═══╝─╚╝─╚╝─╚╩╝─╚╩═══╝
 
 "
 echo Starting dependency installation in 5 seconds...
@@ -17,6 +16,6 @@ apt-get update
 apt-get upgrade -y
 pkg upgrade -y
 pkg install python wget -y
-wget https://raw.githubusercontent.com/Javes786/Lethal/master/Lethal-setup.py
+wget https://raw.githubusercontent.com/Javes786/LETHAL-USERBOT/main/Lethal-setup.py
 pip install telethon
 python Lethal-setup.py
