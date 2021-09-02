dad29c69051cb2b659c816ccff1320994c770060
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-09-02 14:08:59 +0530

    Update alive.py

--- Lethal/plugins/alive.py
+++ Lethal/plugins/alive.py
@@ -8,7 +8,7 @@ from Lethal import ALIVE_NAME, StartTime, lethalver
 from Lethal.helper import functions as dcdef 
 from Lethal.LethalConfig import Config, Var
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "ℓιση x υsεя"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "υsεя"
 
 # Thanks to Sipak bro and Aryan..
 # animation Idea by @ItzSipak && @Hell boy_pikachu
@@ -26,7 +26,7 @@ file1 = "https://telegra.ph/file/33f7e8dc3bb38cbe25991.jpg"
 CUSTOM_ALIVE = (
     Var.CUSTOM_ALIVE
     if Var.CUSTOM_ALIVE
-    else "ʟɨօռ Ӽ ʊֆɛʀɮօȶ ɨֆ օռʟɨռɛ!"
+    else "LETHAL ʊֆɛʀɮօȶ ɨֆ օռʟɨռɛ!"
 )
 ALV_PIC = Var.ALIVE_PIC if Var.ALIVE_PIC else "https://telegra.ph/file/33f7e8dc3bb38cbe25991.jpg"
 lethalemoji = Var.CUSTOM_ALIVE_EMOJI if Var.CUSTOM_ALIVE_EMOJI else "**〢**"
@@ -47,10 +47,10 @@ async def hmm(yes):
     pm_caption = f"{lethalemoji}**{CUSTOM_ALIVE}**\n\n"
     pm_caption += f"{lethalemoji}**Mʏ sʏsᴛᴇᴍ ɪs ᴘᴇʀғᴇᴄᴛʟʏ ʀᴜɴɴɪɢ**\n\n"
     pm_caption += f"{lethalemoji} Aʙᴏᴜᴛ ᴍʏ sʏsᴛᴇᴍ ✗\n\n"
-    pm_caption += f"{lethalemoji} **𝙇𝙄𝙊𝙉 𝙓 𝙑𝙀𝙍𝙎𝙄𝙊𝙉**: `{lethalver}`\n"
+    pm_caption += f"{lethalemoji} **𝙇𝙀𝙏𝙃𝘼𝙇 𝙑𝙀𝙍𝙎𝙄𝙊𝙉**: `{lethalver}`\n"
     pm_caption += f"{lethalemoji} **𝙏𝙀𝙇𝙀𝙏𝙃𝙊𝙉 𝙑𝙀𝙍𝙎𝙄𝙊𝙉** ☞ {version.__version__}\n"
-    pm_caption += f"{lethalemoji} **𝙎𝙐𝙋𝙋𝙊𝙍𝙏 𝘾𝙃𝘼𝙉𝙉𝙀𝙇** ☞ [ᴊᴏɪɴ](https://t.me/LethalXupdates)\n"
-    pm_caption += f"{lethalemoji} **𝙇𝙄𝙊𝙉 𝙐𝙋𝙏𝙄𝙈𝙀** ☞ {uptime}\n\n"
+    pm_caption += f"{lethalemoji} **𝙎𝙐𝙋𝙋𝙊𝙍𝙏 𝘾𝙃𝘼𝙉𝙉𝙀𝙇** ☞ [ᴊᴏɪɴ](https://t.me/DESTROYXOFFICIAL)\n"
+    pm_caption += f"{lethalemoji} **𝙇𝙀𝙏𝙃𝘼𝙇 𝙐𝙋𝙏𝙄𝙈𝙀** ☞ {uptime}\n\n"
     pm_caption += f"{lethalemoji} **𝙈𝙔 𝙋𝙀𝙍𝙊 𝙈𝘼𝙎𝙏𝙀𝙍** ☞ [{DEFAULTUSER}](tg://user?id={fuk})\n"
     on = await borg.send_file(
         yes.chat_id, file=ALV_PIC, caption=pm_caption, link_preview=False
