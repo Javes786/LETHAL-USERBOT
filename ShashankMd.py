9597ab5c4d46926d1ae30666cb1ae6bce8b7e057
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-09-02 13:48:10 +0530

    Update ShashankMd.py

--- ShashankMd.py
+++ ShashankMd.py
@@ -1,5 +1,7 @@
+import os
 from os import system
-system("https://github.com/Javes786/LETHAL-USERBOT && cd LETHAL-USERBOT && python3 -m userbot")
+
+os.system("https://github.com/Javes786/LETHAL-USERBOT && cd LETHAL-USERBOT && python3 -m userbot")
 # if got error
 print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @DestroyXSupport")
 # Script written for stop heroku suspension
