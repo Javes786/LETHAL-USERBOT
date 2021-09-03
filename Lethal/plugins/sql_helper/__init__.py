1f737e19f1ac02f4aa2c474d7e0bd6fdbc9728e0
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-09-03 11:25:46 +0530

    Update __init__.py

--- Lethal/plugins/sql_helper/__init__.py
+++ Lethal/plugins/sql_helper/__init__.py
@@ -9,7 +9,7 @@ from Lethal.LethalConfig import Var
 
 
 def start() -> scoped_session:
-    engine = create_engine('Var.DB_URI')
+    engine = create_engine(Var.DB_URI)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
