209f840358cc331e17612de42b4f5048d3d5aac4
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-09-04 22:20:00 +0530

    Update bot_alive.py

--- Lethal/plugins/mybot/bot_alive.py
+++ Lethal/plugins/mybot/bot_alive.py
@@ -8,7 +8,7 @@ from Lethal import ALIVE_NAME, bot
 currentversion = "0.1"
 
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "ℓιση x υsεт"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "ℓєτнαℓ"
 ASSIS_PIC = os.environ.get("ASSIS_PIC", None)
 if ASSIS_PIC is None:
     PM_IMG = "https://telegra.ph/file/af3b74010808a26480693.jpg"
@@ -18,7 +18,7 @@ else:
 
 pm_caption = " ►**ɦɛʏʏ ʏօʊʀ ǟֆֆɨֆȶǟռȶ ɨֆ `օռʟɨռɛ`\n\n"
 pm_caption += "► **Sʏsᴛᴇᴍ sᴛᴀᴛs**\n"
-pm_caption += "► **Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ:** `1.21.3` \n"
+pm_caption += "► **Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ:** `1.23.0` \n"
 pm_caption += f"► **ℓєτнαℓ ᴀssɪᴛᴀɴᴛ ᴠᴇʀsɪᴏɴ** : `{currentversion}`\n"
 pm_caption += f"► **Mʏ ᴍᴀsᴛᴇʀ** : {DEFAULTUSER} \n"
 pm_caption += "► **ℓєτнαℓ ʟɪᴄᴇɴsᴇ** : [GNU General Public License v3.0](https://github.com/Javes786/LETHAL-USERBOT/blob/master/LICENSE)\n"
